7d8c6d418dd0cf1f9519b63fad1a163854e0df39
AndresArdila89 <andresardila@me.com>
2020-11-24 23:42:35 -0500

    list iteration

---
+++ list_inside_list.py
@@ -0,0 +1,8 @@
+"""
+    Lists Iteration for loop
+    Author:Andres Ardila 
+    Textbook Guide: Head First Python
+    Date: nov-24-2020 21:28h
+"""
+
+fav_movies = ["The Holy Grail", "The Life of Brian"]
