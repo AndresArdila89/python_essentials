69f48be6d7e5524ce4def2a73e0fc31d8a542b04
AndresArdila89 <andresardila@me.com>
2020-11-26 23:30:00 -0500

    build a module

---
+++ nester/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='nester',
+    version='1.0.0',
+    py_modules=['nester'],
+    author='hfpython',
+    author_email='miemail@hotmail.com',
+    url='null',
+    description='A simple printer of nested lists',
+)
